73edc18e6f23eb7cd37133e4afbe65d7f1de40bf
ly1999-hub <nhly123123456789@gmail.com>
2022-11-22 00:30:54 +0700

    log response

---
+++ back-end/log/response.js
@@ -0,0 +1,33 @@
+module.exports.Status={
+    Status200:{
+        index:1,
+        code:200,
+        description:"ok"
+    },
+
+    Status400:{
+        index:0,
+        code:400,
+        description:"bad request"
+    },
+
+    Status404:{
+        index:0,
+        code:404,
+        description:"page not found"
+    },
+
+    Status500:{
+        index:0,
+        code:500,
+        description:"internal service error"
+    },
+
+    Status503:{
+        index:0,
+        code:503,
+        description:"service unavailable"
+    }
+    
+}
+    
