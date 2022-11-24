451fbef7f188767b64bad8c23906a1dbd76953bd
ly1999-hub <nhly123123456789@gmail.com>
2022-11-25 00:37:33 +0700

    import bodyparse

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express();
+var cors = require('cors')
 const InitDB = require('./models/initMongoDB')
+const bodyParser = require('body-parser')
 
 InitDB.initMongoDB()
 
-const port = 3000;
+const port = 8080;
 const attire = require('./routers/attire')
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors())
 app.use('/attire', attire);
 
 app.listen(port, () => {
