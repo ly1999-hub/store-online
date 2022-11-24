baf6616753afb8b81538a9105f9bae8f9d134b99
ly1999-hub <nhly123123456789@gmail.com>
2022-11-25 00:41:43 +0700

    install module

--- front-end/src/compoment/menu.js
+++ front-end/src/compoment/menu.js
@@ -1,6 +1,27 @@
-export default function menu(){
+import {Nav} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../static/style/menu.css'
+
+function  Menu() {
+
     return(
-    <div>
-        
-    </div>)
+            <Nav>
+                <Nav.Item>
+                    <Nav.Link href={"/register"}> Register </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href={"/todolist"}> Todolist </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href={"/login"}> Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="icon-center">
+                    <FontAwesomeIcon icon={faUser} />
+                </Nav.Item>
+                
+            </Nav>
+        )
 }
+export default Menu;
